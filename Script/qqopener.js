22cc82a37182dfa9f1f9509a9c0079663042703c
mudori <71554552+mudori@users.noreply.github.com>
2021-02-07 12:44:52 +0800

    增加一些脚本

---
+++ Script/qqopener.js
@@ -0,0 +1,46 @@
+/*
+[rewrite_local]
+
+^htps:\/\/qr\.alipay\.com\/[a-zA-Z0-9]{22} url script-request-header https://raw.githubusercontent.com/barrym-chen/Script/master/qqopener/qqopener.js
+
+^http:\/\/c\.pc\.qq\.com\/middlem\.html url script-request-header https://raw.githubusercontent.com/barrym-chen/Script/master/qqopener/qqopener.js
+**********************
+Surge 4.2.0+ 脚本配置:
+**********************
+[Script]
+qqopener.js = type=http-request,pattern=^http:\/\/c\.pc\.qq\.com\/middlem\.html,script-path=https://raw.githubusercontent.com/barrym-chen/Script/master/qqopener/qqopener.js
+qqalipay.js=type=http-request,pattern=htps:\/\/qr\.alipay\.com\/[a-zA-Z0-9]{22},script-path=https://raw.githubusercontent.com/barrym-chen/Script/master/qqopener/qqopener.js
+
+by @barry
+原作者@江湖中人,修改而来。
+*/
+var str = ($request.url);
+function test(){
+stre=str.split("=")
+res=stre[1]
+s=res.split("&")
+st=s[0]
+reurl=decodeURIComponent(st)
+}
+//console.log(reurl)
+
+const $ = new cmp()
+
+if(str.indexOf("alipay") != -1){
+  $.notify(``,"", "🔗点击支付宝链接", str)
+}else{
+test()
+  $.notify(``,"", "🔗点击打开链接", reurl)
+}
+$done();
+
+function cmp() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
+    }
+}
