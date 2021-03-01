fa0d59dd068346e379b0c7df9eb36d83a752b70e
mudori <71554552+mudori@users.noreply.github.com>
2021-03-01 19:59:12 +0800

    debug: delete media-url

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -131,8 +131,6 @@ async function scheduler() {
   await query();
   weatherAlert();
   realtimeWeather();
-  // hourlyForcast();
-  // dailyForcast();
 }
 
 async function query() {
@@ -257,13 +255,13 @@ function realtimeWeather() {
     const now = dt.getHours() + 1;
     dt.setHours(dt.getHours() + 1);
     hourlySkycon +=
-      `${now}-${dt.getHours() + 1}时 ${mapSkycon(skycon.value)[0]}` +
+      `${now}-${dt.getHours() + 1}时 ${mapSkycon(skycon.value)}` +
       (i == 2 ? "" : "\n");
   }
 
   $.notify(
     `[彩云天气] ${address.city} ${address.district} ${address.street}`,
-    `${mapSkycon(realtime.skycon)[0]} ${realtime.temperature} ℃  🌤 空气质量 ${
+    `${mapSkycon(realtime.skycon)} ${realtime.temperature} ℃  🌤 空气质量 ${
       realtime.air_quality.description.chn
     }`,
     `🔱 ${keypoint}
@@ -276,14 +274,9 @@ function realtimeWeather() {
     )}
 
 ${alertInfo}${hourlySkycon}
-`,
-    {
-      "media-url": `${mapSkycon(realtime.skycon)[1]}`,
-    }
+`
   );
 }
-
-function dailyForcast() {}
 
 /************************** 天气对照表 *********************************/
 
@@ -399,73 +392,24 @@ function mapWind(speed, direction) {
 }
 
 // 天气状况 --> 自然语言描述
-// icon来源：github@58xinian
 function mapSkycon(skycon) {
   const map = {
-    CLEAR_DAY: [
-      "☀️ 日间晴朗",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/CLEAR_DAY.gif",
-    ],
-    CLEAR_NIGHT: [
-      "✨ 夜间晴朗",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/CLEAR_NIGHT.gif",
-    ],
-    PARTLY_CLOUDY_DAY: [
-      "⛅️ 日间多云",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/PARTLY_CLOUDY_DAY.gif",
-    ],
-    PARTLY_CLOUDY_NIGHT: [
-      "☁️ 夜间多云",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/PARTLY_CLOUDY_NIGHT.gif",
-    ],
-    CLOUDY: [
-      "☁️ 阴",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/CLOUDY.gif",
-    ],
-    LIGHT_HAZE: [
-      "😤 轻度雾霾",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/HAZE.gif",
-    ],
-    MODERATE_HAZE: [
-      "😤 中度雾霾",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/HAZE.gif",
-    ],
-    HEAVY_HAZE: [
-      "😤 重度雾霾",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/HAZE.gif",
-    ],
-    LIGHT_RAIN: [
-      "💧 小雨",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/LIGHT.gif",
-    ],
-    MODERATE_RAIN: [
-      "💦 中雨",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/MODERATE_RAIN.gif",
-    ],
-    HEAVY_RAIN: [
-      "🌧 大雨",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/STORM_RAIN.gif",
-    ],
-    STORM_RAIN: [
-      "⛈ 暴雨",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/STORM_RAIN.gif",
-    ],
-    LIGHT_SNOW: [
-      "🌨 小雪",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/LIGHT_SNOW.gif",
-    ],
-    MODERATE_SNOW: [
-      "❄️ 中雪",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/MODERATE_SNOW.gif",
-    ],
-    HEAVY_SNOW: [
-      "☃️ 大雪",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/HEAVY_SNOW.gif",
-    ],
-    STORM_SNOW: [
-      "⛄️暴雪",
-      "https://raw.githubusercontent.com/58xinian/icon/master/Weather/HEAVY_SNOW",
-    ],
+    CLEAR_DAY: ["☀️ 日间晴朗"],
+    CLEAR_NIGHT: ["✨ 夜间晴朗"],
+    PARTLY_CLOUDY_DAY: ["⛅️ 日间多云"],
+    PARTLY_CLOUDY_NIGHT: ["☁️ 夜间多云"],
+    CLOUDY: ["☁️ 阴"],
+    LIGHT_HAZE: ["😤 轻度雾霾"],
+    MODERATE_HAZE: ["😤 中度雾霾"],
+    HEAVY_HAZE: ["😤 重度雾霾"],
+    LIGHT_RAIN: ["💧 小雨"],
+    MODERATE_RAIN: ["💦 中雨"],
+    HEAVY_RAIN: ["🌧 大雨"],
+    STORM_RAIN: ["⛈ 暴雨"],
+    LIGHT_SNOW: ["🌨 小雪"],
+    MODERATE_SNOW: ["❄️ 中雪"],
+    HEAVY_SNOW: ["☃️ 大雪"],
+    STORM_SNOW: ["⛄️暴雪"],
     FOG: ["🌫️ 雾"],
     DUST: ["💨 浮尘"],
     SAND: ["💨 沙尘"],
@@ -507,4 +451,3 @@ function MYERR() {
 /*********************************** API *************************************/
 function ENV(){const e="undefined"!=typeof $task,t="undefined"!=typeof $loon,s="undefined"!=typeof $httpClient&&!t,i="function"==typeof require&&"undefined"!=typeof $jsbox;return{isQX:e,isLoon:t,isSurge:s,isNode:"function"==typeof require&&!i,isJSBox:i,isRequest:"undefined"!=typeof $request,isScriptable:"undefined"!=typeof importModule}}function HTTP(e={baseURL:""}){const{isQX:t,isLoon:s,isSurge:i,isScriptable:n,isNode:o}=ENV(),r=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/;const u={};return["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].forEach(l=>u[l.toLowerCase()]=(u=>(function(u,l){l="string"==typeof l?{url:l}:l;const h=e.baseURL;h&&!r.test(l.url||"")&&(l.url=h?h+l.url:l.url);const a=(l={...e,...l}).timeout,c={onRequest:()=>{},onResponse:e=>e,onTimeout:()=>{},...l.events};let f,d;if(c.onRequest(u,l),t)f=$task.fetch({method:u,...l});else if(s||i||o)f=new Promise((e,t)=>{(o?require("request"):$httpClient)[u.toLowerCase()](l,(s,i,n)=>{s?t(s):e({statusCode:i.status||i.statusCode,headers:i.headers,body:n})})});else if(n){const e=new Request(l.url);e.method=u,e.headers=l.headers,e.body=l.body,f=new Promise((t,s)=>{e.loadString().then(s=>{t({statusCode:e.response.statusCode,headers:e.response.headers,body:s})}).catch(e=>s(e))})}const p=a?new Promise((e,t)=>{d=setTimeout(()=>(c.onTimeout(),t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)),a)}):null;return(p?Promise.race([p,f]).then(e=>(clearTimeout(d),e)):f).then(e=>c.onResponse(e))})(l,u))),u}function API(e="untitled",t=!1){const{isQX:s,isLoon:i,isSurge:n,isNode:o,isJSBox:r,isScriptable:u}=ENV();return new class{constructor(e,t){this.name=e,this.debug=t,this.http=HTTP(),this.env=ENV(),this.node=(()=>{if(o){return{fs:require("fs")}}return null})(),this.initCache();Promise.prototype.delay=function(e){return this.then(function(t){return((e,t)=>new Promise(function(s){setTimeout(s.bind(null,t),e)}))(e,t)})}}initCache(){if(s&&(this.cache=JSON.parse($prefs.valueForKey(this.name)||"{}")),(i||n)&&(this.cache=JSON.parse($persistentStore.read(this.name)||"{}")),o){let e="root.json";this.node.fs.existsSync(e)||this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.root={},e=`${this.name}.json`,this.node.fs.existsSync(e)?this.cache=JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)):(this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.cache={})}}persistCache(){const e=JSON.stringify(this.cache,null,2);s&&$prefs.setValueForKey(e,this.name),(i||n)&&$persistentStore.write(e,this.name),o&&(this.node.fs.writeFileSync(`${this.name}.json`,e,{flag:"w"},e=>console.log(e)),this.node.fs.writeFileSync("root.json",JSON.stringify(this.root,null,2),{flag:"w"},e=>console.log(e)))}write(e,t){if(this.log(`SET ${t}`),-1!==t.indexOf("#")){if(t=t.substr(1),n||i)return $persistentStore.write(e,t);if(s)return $prefs.setValueForKey(e,t);o&&(this.root[t]=e)}else this.cache[t]=e;this.persistCache()}read(e){return this.log(`READ ${e}`),-1===e.indexOf("#")?this.cache[e]:(e=e.substr(1),n||i?$persistentStore.read(e):s?$prefs.valueForKey(e):o?this.root[e]:void 0)}delete(e){if(this.log(`DELETE ${e}`),-1!==e.indexOf("#")){if(e=e.substr(1),n||i)return $persistentStore.write(null,e);if(s)return $prefs.removeValueForKey(e);o&&delete this.root[e]}else delete this.cache[e];this.persistCache()}notify(e,t="",l="",h={}){const a=h["open-url"];if(s&&$notify(e,t,l,h),n&&$notification.post(e,t,l,{url:a}),i){let s={};a&&(s.openUrl=a),"{}"===JSON.stringify(s)?$notification.post(e,t,l):$notification.post(e,t,l,s)}if(o||u){const s=l+(a?`\n点击跳转: ${a}`:"");if(r){require("push").schedule({title:e,body:(t?t+"\n":"")+s})}else console.log(`${e}\n${t}\n${s}\n\n`)}}log(e){this.debug&&console.log(`[${this.name}] LOG: ${this.stringify(e)}`)}info(e){console.log(`[${this.name}] INFO: ${this.stringify(e)}`)}error(e){console.log(`[${this.name}] ERROR: ${this.stringify(e)}`)}wait(e){return new Promise(t=>setTimeout(t,e))}done(e={}){s||i||n?$done(e):o&&!r&&"undefined"!=typeof $context&&($context.headers=e.headers,$context.statusCode=e.statusCode,$context.body=e.body)}stringify(e){if("string"==typeof e||e instanceof String)return e;try{return JSON.stringify(e,null,2)}catch(e){return"[object Object]"}}}(e,t)}
 /*****************************************************************************/
-
