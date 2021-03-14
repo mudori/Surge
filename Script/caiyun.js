9f29ab93124671a4d4bfb6817ecc399e51a34e1c
mudori <71554552+mudori@users.noreply.github.com>
2021-03-14 22:46:00 +0800

    彩云天气

--- Script/caiyun.js
+++ Script/caiyun.js
@@ -124,8 +124,7 @@ if (typeof $request !== "undefined") {
 async function scheduler() {
   const now = new Date();
   $.log(
-    `Scheduler activated at ${
-      now.getMonth() + 1
+    `Scheduler activated at ${now.getMonth() + 1
     }月${now.getDate()}日${now.getHours()}时${now.getMinutes()}分`
   );
   await query();
@@ -147,11 +146,9 @@ async function query() {
     );
   }
   // query API
-  const url = `https://api.caiyunapp.com/v2.5/${$.read("token").caiyun}/${
-    $.read("location").longitude
-  },${
-    $.read("location").latitude
-  }/weather?lang=zh_CN&dailystart=0&hourlysteps=384&dailysteps=16&alert=true`;
+  const url = `https://api.caiyunapp.com/v2.5/${$.read("token").caiyun}/${$.read("location").longitude
+    },${$.read("location").latitude
+    }/weather?lang=zh_CN&dailystart=0&hourlysteps=384&dailysteps=16&alert=true&&unit=metric:v2`;
 
   $.log("Query weather...");
 
@@ -180,8 +177,7 @@ async function query() {
     await $.wait(Math.random() * 2000);
     $.log("Query location...");
     address = await $.http.get(
-      `https://apis.map.qq.com/ws/geocoder/v1/?key=${
-        $.read("token").tencent
+      `https://apis.map.qq.com/ws/geocoder/v1/?key=${$.read("token").tencent
       }&location=${$.read("location").latitude},${$.read("location").longitude}`
     )
       .then((resp) => {
@@ -236,46 +232,61 @@ function realtimeWeather() {
     alert.content.length == 0
       ? ""
       : alert.content.reduce((acc, curr) => {
-          if (curr.status === "预警中") {
-            return acc + "\n" + mapAlertCode(curr.code) + "预警";
-          } else {
-            return acc;
-          }
-        }, "[预警]") + "\n\n";
+        if (curr.status === "预警中") {
+          return acc + "\n" + mapAlertCode(curr.code) + "预警";
+        } else {
+          return acc;
+        }
+      }, "[预警]");
 
   const realtime = data.realtime;
-  const keypoint = data.forecast_keypoint;
-
+  const minutely = data.minutely;
   const hourly = data.hourly;
+  const daily = data.daily;
 
-  let hourlySkycon = "[未来3小时]\n";
-  for (let i = 0; i < 3; i++) {
-    const skycon = hourly.skycon[i];
-    const dt = new Date(skycon.datetime);
-    const now = dt.getHours() + 1;
-    dt.setHours(dt.getHours() + 1);
-    hourlySkycon +=
-      `${now}-${dt.getHours() + 1}时 ${mapSkycon(skycon.value)}` +
+  // let hourlySkycon = "[未来4小时]\n";
+  // for (let i = 0; i < 4; i++) {
+  //   const skycon = hourly.skycon[i];
+  //   const temperature = hourly.temperature[i];
+  //   const dt = new Date(skycon.datetime);
+  //   const now = dt.getHours() + 1;
+  //   dt.setHours(dt.getHours() + 1);
+  //   hourlySkycon +=
+  //     `${now}-${dt.getHours() + 1}时 ${mapSkycon(skycon.value)} ${temperature.value}℃` +
+  //     (i == 3 ? "" : "\n");
+  // }
+
+  let dailySkycon = "[未来2天]\n";
+  for (let i = 1; i < 3; i++) {
+    const skycon = daily.skycon[i];
+    const temperature = daily.temperature[i];
+    const dt = new Date(temperature.date);
+    const day = dt.toLocaleDateString();
+    dailySkycon +=
+      `${day} ${mapSkycon(skycon.value)} ${temperature.min}℃ - ${temperature.max}℃` +
       (i == 2 ? "" : "\n");
   }
 
+
   $.notify(
     `[彩云天气] ${address.city} ${address.district} ${address.street}`,
-    `${mapSkycon(realtime.skycon)} ${realtime.temperature} ℃  🌤 空气质量 ${
-      realtime.air_quality.description.chn
-    }`,
-    `🔱 ${keypoint}
-🌡 体感${realtime.life_index.comfort.desc} ${
-      realtime.apparent_temperature
-    } ℃  💧 湿度 ${(realtime.humidity * 100).toFixed(0)}%
+    `${mapSkycon(realtime.skycon)} ${realtime.temperature}℃  🌤 空气质量 ${realtime.air_quality.description.usa}`,
+    `PM2.5浓度 ${realtime.air_quality.pm25}μg/m3  AQI ${realtime.air_quality.aqi.usa}
+🌡 体感 ${realtime.apparent_temperature}℃  💧 湿度 ${(realtime.humidity * 100).toFixed(0)}%
+👀 能见度 ${realtime.visibility}km  降水强度 ${realtime.precipitation.local.intensity}mm/h
 🌞 紫外线 ${realtime.life_index.ultraviolet.desc} 💨 ${mapWind(
       realtime.wind.speed,
       realtime.wind.direction
     )}
-
-${alertInfo}${hourlySkycon}`
+[未来2小时]
+🔱 ${minutely.description}
+降水 ${(minutely.probability[0] * 100).toFixed(0)}%-${minutely.precipitation_2h[29]},  ${(minutely.probability[1] * 100).toFixed(0)}%-${minutely.precipitation_2h[59]},  ${(minutely.probability[2] * 100).toFixed(0)}%-${minutely.precipitation_2h[89]},  ${(minutely.probability[3] * 100).toFixed(0)}%-${minutely.precipitation_2h[119]}
+${alertInfo}${dailySkycon}`
   );
 }
+// 🌡 体感${realtime.life_index.comfort.desc} ${realtime.apparent_temperature}℃  💧 湿度 ${(realtime.humidity * 100).toFixed(0)}%
+// 📏 最近的降水带在${ realtime.precipitation.nearest.distance } 公里外呢
+// ${hourlySkycon}
 
 /************************** 天气对照表 *********************************/
 
@@ -393,9 +404,9 @@ function mapWind(speed, direction) {
 // 天气状况 --> 自然语言描述
 function mapSkycon(skycon) {
   const map = {
-    CLEAR_DAY: ["☀️ 日间晴朗"],
+    CLEAR_DAY: ["☀️ 白天晴朗"],
     CLEAR_NIGHT: ["✨ 夜间晴朗"],
-    PARTLY_CLOUDY_DAY: ["⛅️ 日间多云"],
+    PARTLY_CLOUDY_DAY: ["⛅️ 白天多云"],
     PARTLY_CLOUDY_NIGHT: ["☁️ 夜间多云"],
     CLOUDY: ["☁️ 阴"],
     LIGHT_HAZE: ["😤 轻度雾霾"],
@@ -405,32 +416,17 @@ function mapSkycon(skycon) {
     MODERATE_RAIN: ["💦 中雨"],
     HEAVY_RAIN: ["🌧 大雨"],
     STORM_RAIN: ["⛈ 暴雨"],
+    FOG: ["🌫️ 雾"],
     LIGHT_SNOW: ["🌨 小雪"],
     MODERATE_SNOW: ["❄️ 中雪"],
     HEAVY_SNOW: ["☃️ 大雪"],
     STORM_SNOW: ["⛄️暴雪"],
-    FOG: ["🌫️ 雾"],
     DUST: ["💨 浮尘"],
     SAND: ["💨 沙尘"],
     WIND: ["🌪 大风"],
   };
   return map[skycon];
 }
-
-// 雷达降 水/雪 强度 --> skycon
-function mapPrecipitation(intensity) {
-  if (0.031 < intensity && intensity < 0.25) {
-    return "LIGHT";
-  } else if (intensity < 0.35) {
-    return "MODERATE";
-  } else if (intensity < 0.48) {
-    return "HEADY";
-  } else if (intensity >= 0.48) {
-    return "STORM";
-  }
-}
-
-function mapIntensity(breakpoints) {}
 
 /************************** ERROR *********************************/
 function MYERR() {
@@ -448,5 +444,5 @@ function MYERR() {
 
 // prettier-ignore
 /*********************************** API *************************************/
-function ENV(){const e="undefined"!=typeof $task,t="undefined"!=typeof $loon,s="undefined"!=typeof $httpClient&&!t,i="function"==typeof require&&"undefined"!=typeof $jsbox;return{isQX:e,isLoon:t,isSurge:s,isNode:"function"==typeof require&&!i,isJSBox:i,isRequest:"undefined"!=typeof $request,isScriptable:"undefined"!=typeof importModule}}function HTTP(e={baseURL:""}){const{isQX:t,isLoon:s,isSurge:i,isScriptable:n,isNode:o}=ENV(),r=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/;const u={};return["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].forEach(l=>u[l.toLowerCase()]=(u=>(function(u,l){l="string"==typeof l?{url:l}:l;const h=e.baseURL;h&&!r.test(l.url||"")&&(l.url=h?h+l.url:l.url);const a=(l={...e,...l}).timeout,c={onRequest:()=>{},onResponse:e=>e,onTimeout:()=>{},...l.events};let f,d;if(c.onRequest(u,l),t)f=$task.fetch({method:u,...l});else if(s||i||o)f=new Promise((e,t)=>{(o?require("request"):$httpClient)[u.toLowerCase()](l,(s,i,n)=>{s?t(s):e({statusCode:i.status||i.statusCode,headers:i.headers,body:n})})});else if(n){const e=new Request(l.url);e.method=u,e.headers=l.headers,e.body=l.body,f=new Promise((t,s)=>{e.loadString().then(s=>{t({statusCode:e.response.statusCode,headers:e.response.headers,body:s})}).catch(e=>s(e))})}const p=a?new Promise((e,t)=>{d=setTimeout(()=>(c.onTimeout(),t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)),a)}):null;return(p?Promise.race([p,f]).then(e=>(clearTimeout(d),e)):f).then(e=>c.onResponse(e))})(l,u))),u}function API(e="untitled",t=!1){const{isQX:s,isLoon:i,isSurge:n,isNode:o,isJSBox:r,isScriptable:u}=ENV();return new class{constructor(e,t){this.name=e,this.debug=t,this.http=HTTP(),this.env=ENV(),this.node=(()=>{if(o){return{fs:require("fs")}}return null})(),this.initCache();Promise.prototype.delay=function(e){return this.then(function(t){return((e,t)=>new Promise(function(s){setTimeout(s.bind(null,t),e)}))(e,t)})}}initCache(){if(s&&(this.cache=JSON.parse($prefs.valueForKey(this.name)||"{}")),(i||n)&&(this.cache=JSON.parse($persistentStore.read(this.name)||"{}")),o){let e="root.json";this.node.fs.existsSync(e)||this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.root={},e=`${this.name}.json`,this.node.fs.existsSync(e)?this.cache=JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)):(this.node.fs.writeFileSync(e,JSON.stringify({}),{flag:"wx"},e=>console.log(e)),this.cache={})}}persistCache(){const e=JSON.stringify(this.cache,null,2);s&&$prefs.setValueForKey(e,this.name),(i||n)&&$persistentStore.write(e,this.name),o&&(this.node.fs.writeFileSync(`${this.name}.json`,e,{flag:"w"},e=>console.log(e)),this.node.fs.writeFileSync("root.json",JSON.stringify(this.root,null,2),{flag:"w"},e=>console.log(e)))}write(e,t){if(this.log(`SET ${t}`),-1!==t.indexOf("#")){if(t=t.substr(1),n||i)return $persistentStore.write(e,t);if(s)return $prefs.setValueForKey(e,t);o&&(this.root[t]=e)}else this.cache[t]=e;this.persistCache()}read(e){return this.log(`READ ${e}`),-1===e.indexOf("#")?this.cache[e]:(e=e.substr(1),n||i?$persistentStore.read(e):s?$prefs.valueForKey(e):o?this.root[e]:void 0)}delete(e){if(this.log(`DELETE ${e}`),-1!==e.indexOf("#")){if(e=e.substr(1),n||i)return $persistentStore.write(null,e);if(s)return $prefs.removeValueForKey(e);o&&delete this.root[e]}else delete this.cache[e];this.persistCache()}notify(e,t="",l="",h={}){const a=h["open-url"];if(s&&$notify(e,t,l,h),n&&$notification.post(e,t,l,{url:a}),i){let s={};a&&(s.openUrl=a),"{}"===JSON.stringify(s)?$notification.post(e,t,l):$notification.post(e,t,l,s)}if(o||u){const s=l+(a?`\n点击跳转: ${a}`:"");if(r){require("push").schedule({title:e,body:(t?t+"\n":"")+s})}else console.log(`${e}\n${t}\n${s}\n\n`)}}log(e){this.debug&&console.log(`[${this.name}] LOG: ${this.stringify(e)}`)}info(e){console.log(`[${this.name}] INFO: ${this.stringify(e)}`)}error(e){console.log(`[${this.name}] ERROR: ${this.stringify(e)}`)}wait(e){return new Promise(t=>setTimeout(t,e))}done(e={}){s||i||n?$done(e):o&&!r&&"undefined"!=typeof $context&&($context.headers=e.headers,$context.statusCode=e.statusCode,$context.body=e.body)}stringify(e){if("string"==typeof e||e instanceof String)return e;try{return JSON.stringify(e,null,2)}catch(e){return"[object Object]"}}}(e,t)}
+function ENV() { const e = "undefined" != typeof $task, t = "undefined" != typeof $loon, s = "undefined" != typeof $httpClient && !t, i = "function" == typeof require && "undefined" != typeof $jsbox; return { isQX: e, isLoon: t, isSurge: s, isNode: "function" == typeof require && !i, isJSBox: i, isRequest: "undefined" != typeof $request, isScriptable: "undefined" != typeof importModule } } function HTTP(e = { baseURL: "" }) { const { isQX: t, isLoon: s, isSurge: i, isScriptable: n, isNode: o } = ENV(), r = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/; const u = {}; return ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"].forEach(l => u[l.toLowerCase()] = (u => (function (u, l) { l = "string" == typeof l ? { url: l } : l; const h = e.baseURL; h && !r.test(l.url || "") && (l.url = h ? h + l.url : l.url); const a = (l = { ...e, ...l }).timeout, c = { onRequest: () => { }, onResponse: e => e, onTimeout: () => { }, ...l.events }; let f, d; if (c.onRequest(u, l), t) f = $task.fetch({ method: u, ...l }); else if (s || i || o) f = new Promise((e, t) => { (o ? require("request") : $httpClient)[u.toLowerCase()](l, (s, i, n) => { s ? t(s) : e({ statusCode: i.status || i.statusCode, headers: i.headers, body: n }) }) }); else if (n) { const e = new Request(l.url); e.method = u, e.headers = l.headers, e.body = l.body, f = new Promise((t, s) => { e.loadString().then(s => { t({ statusCode: e.response.statusCode, headers: e.response.headers, body: s }) }).catch(e => s(e)) }) } const p = a ? new Promise((e, t) => { d = setTimeout(() => (c.onTimeout(), t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)), a) }) : null; return (p ? Promise.race([p, f]).then(e => (clearTimeout(d), e)) : f).then(e => c.onResponse(e)) })(l, u))), u } function API(e = "untitled", t = !1) { const { isQX: s, isLoon: i, isSurge: n, isNode: o, isJSBox: r, isScriptable: u } = ENV(); return new class { constructor(e, t) { this.name = e, this.debug = t, this.http = HTTP(), this.env = ENV(), this.node = (() => { if (o) { return { fs: require("fs") } } return null })(), this.initCache(); Promise.prototype.delay = function (e) { return this.then(function (t) { return ((e, t) => new Promise(function (s) { setTimeout(s.bind(null, t), e) }))(e, t) }) } } initCache() { if (s && (this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}")), (i || n) && (this.cache = JSON.parse($persistentStore.read(this.name) || "{}")), o) { let e = "root.json"; this.node.fs.existsSync(e) || this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.root = {}, e = `${this.name}.json`, this.node.fs.existsSync(e) ? this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)) : (this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.cache = {}) } } persistCache() { const e = JSON.stringify(this.cache, null, 2); s && $prefs.setValueForKey(e, this.name), (i || n) && $persistentStore.write(e, this.name), o && (this.node.fs.writeFileSync(`${this.name}.json`, e, { flag: "w" }, e => console.log(e)), this.node.fs.writeFileSync("root.json", JSON.stringify(this.root, null, 2), { flag: "w" }, e => console.log(e))) } write(e, t) { if (this.log(`SET ${t}`), -1 !== t.indexOf("#")) { if (t = t.substr(1), n || i) return $persistentStore.write(e, t); if (s) return $prefs.setValueForKey(e, t); o && (this.root[t] = e) } else this.cache[t] = e; this.persistCache() } read(e) { return this.log(`READ ${e}`), -1 === e.indexOf("#") ? this.cache[e] : (e = e.substr(1), n || i ? $persistentStore.read(e) : s ? $prefs.valueForKey(e) : o ? this.root[e] : void 0) } delete(e) { if (this.log(`DELETE ${e}`), -1 !== e.indexOf("#")) { if (e = e.substr(1), n || i) return $persistentStore.write(null, e); if (s) return $prefs.removeValueForKey(e); o && delete this.root[e] } else delete this.cache[e]; this.persistCache() } notify(e, t = "", l = "", h = {}) { const a = h["open-url"]; if (s && $notify(e, t, l, h), n && $notification.post(e, t, l, { url: a }), i) { let s = {}; a && (s.openUrl = a), "{}" === JSON.stringify(s) ? $notification.post(e, t, l) : $notification.post(e, t, l, s) } if (o || u) { const s = l + (a ? `\n点击跳转: ${a}` : ""); if (r) { require("push").schedule({ title: e, body: (t ? t + "\n" : "") + s }) } else console.log(`${e}\n${t}\n${s}\n\n`) } } log(e) { this.debug && console.log(`[${this.name}] LOG: ${this.stringify(e)}`) } info(e) { console.log(`[${this.name}] INFO: ${this.stringify(e)}`) } error(e) { console.log(`[${this.name}] ERROR: ${this.stringify(e)}`) } wait(e) { return new Promise(t => setTimeout(t, e)) } done(e = {}) { s || i || n ? $done(e) : o && !r && "undefined" != typeof $context && ($context.headers = e.headers, $context.statusCode = e.statusCode, $context.body = e.body) } stringify(e) { if ("string" == typeof e || e instanceof String) return e; try { return JSON.stringify(e, null, 2) } catch (e) { return "[object Object]" } } }(e, t) }
 /*****************************************************************************/
